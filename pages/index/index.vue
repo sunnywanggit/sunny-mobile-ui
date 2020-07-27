1451f02a194d1be4230cf9b46d7529bca14e8de8
ergouwang <330035385@qq.com>
2020-07-27 10:43:13 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <view class="content">
+        this is uniapp
+    </view>
+</template>
+
+<script>
+    export default {
+        data() {
+            return { };
+        },
+        onLoad() {
+        },
+        methods: {}
+    };
+</script>
+
+<style> </style>
