a834726f46a17bec3687ba329c3e4816b9db608d
ergouwang <330035385@qq.com>
2020-08-18 16:18:55 +0800

    基本完成 avatar 组件

---
+++ components/SunnyAvatar/SunnyAvatar.vue
@@ -0,0 +1,54 @@
+<template>
+    <view :class="['s-avatar', isShape]" :style="{ height, width }">
+
+
+        <image
+                class="s-avatar__target"
+                :src="src"
+        ></image>
+    </view>
+</template>
+
+<script>
+    export default {
+        name:"SunnyAvatar",
+        props: {
+            src: String,
+            size: { type: Number, default: 80 },
+            shape: { type: String, default: 'circle' }
+        },
+        computed: {
+            isShape() { return `s-avatar--${this.shape}`; },
+
+            height() { return this.size + 'rpx'; },
+
+            width() { return this.size + 'rpx'; },
+
+        },
+    };
+</script>
+<style lang="scss">
+    .s-avatar {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #c0c4cc;
+        font-size: 28rpx;
+        color: #fff;
+        overflow: hidden;
+        box-sizing: border-box;
+
+        &--circle {
+             border-radius: 100%;
+         }
+
+        &--square {
+             border-radius: 10rpx;
+         }
+
+        &__target {
+             height: 100%;
+             width: 100%;
+         }
+    }
+</style>
---
+++ pages/avatar/avatar.vue
@@ -0,0 +1,46 @@
+<template>
+    <view class="demo-avatar">
+        <SunnyCard label="头像形状">
+            <SunnyRow type="flex">
+                <SunnyAvatar :src="url"></SunnyAvatar>
+                <SunnyAvatar :src="url" shape="square"></SunnyAvatar>
+            </SunnyRow>
+        </SunnyCard>
+
+        <SunnyCard label="头像大小">
+            <SunnyRow type="flex">
+                <SunnyAvatar :src="url" :size="70"></SunnyAvatar>
+
+                <SunnyAvatar :src="url" :size="80"></SunnyAvatar>
+
+                <SunnyAvatar :src="url" :size="90"></SunnyAvatar>
+
+                <SunnyAvatar :src="url" :size="100"></SunnyAvatar>
+            </SunnyRow>
+        </SunnyCard>
+    </view>
+</template>
+
+<script>
+    import SunnyCard from '../../components/SunnyCard/SunnyCard';
+    import SunnyRow from '../../components/SunnyRow/SunnyRow';
+    import SunnyAvatar from '../../components/SunnyAvatar/SunnyAvatar';
+    export default {
+        components: {SunnyAvatar, SunnyRow, SunnyCard},
+        data() {
+            return {
+                url:'https://s1.ax1x.com/2020/07/31/aQO84s.png',
+            };
+        }
+    };
+</script>
+
+<style lang="scss">
+    .demo-avatar {
+        .s-row {
+            .s-avatar {
+                margin: 0 20rpx;
+            }
+        }
+    }
+</style>
